90a8415d9b1af3788b88c443a640b42ada5137f6
chaim <gchaimke@gmail.com>
2020-07-25 15:14:47 +0300

    update

--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -6,7 +6,7 @@ if (isset($this->session->userdata['logged_in'])) {
 }
 ?>
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark main-menu"> 
-  <a class="navbar-brand" href="/">Online Forms 6</a>
+  <a class="navbar-brand" href="/">Online Forms 7</a>
   <ul class="navbar-nav mr-auto">
     <?php
     if (isset($project)) {
