592895a11f769e21f7778b872c56c0fe1d878557
gchaim <gchaimke@gmail.com>
2020-07-27 21:20:17 +0300

    v1.4

--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -6,7 +6,7 @@ if (isset($this->session->userdata['logged_in'])) {
 }
 ?>
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark main-menu"> 
-  <a class="navbar-brand" href="/"><?php echo $_SERVER['SERVER_NAME']; ?> - Online Forms v1.3</a>
+  <a class="navbar-brand" href="/"><?php echo $_SERVER['SERVER_NAME']; ?> - Online Forms v1.4</a>
   <ul class="navbar-nav mr-auto">
     <?php
     if (isset($project)) {
