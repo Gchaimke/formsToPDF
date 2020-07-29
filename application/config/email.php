49102349c57d37204ecec631b57524aeec3bafd3
chaim <gchaimke@gmail.com>
2020-07-30 02:49:01 +0300

    email sendmail

--- application/config/email.php
+++ application/config/email.php
@@ -1,10 +1,6 @@
 <?php
 $config['useragent'] = 'CodeIgniter';
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.forms.garin.co.il';
-$config['smtp_user'] = 'forms@forms.garin.co.il';
-$config['smtp_port'] = 993;
-$config['smtp_timeout'] = 5;
+$config['protocol'] = 'sendmail';
 $config['wordwrap'] = TRUE;
 $config['wrapchars'] = 76;
 $config['mailtype'] = 'html';
@@ -16,8 +12,18 @@ $config['newline'] = "\r\n";
 $config['bcc_batch_mode'] = FALSE;
 $config['bcc_batch_size'] = 200;
 
-//GMAIL
-//$config['smtp_host']    = 'ssl://smtp.gmail.com';
-//$config['smtp_user'] = 'user@gmail.com';
-//$config['smtp_pass'] = '*******';
-//$config['smtp_port'] = 465;
+/*
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'mail.forms.garin.co.il';
+$config['smtp_user'] = 'forms@forms.garin.co.il';
+$config['smtp_port'] = 993;
+$config['smtp_timeout'] = 5;
+*/
+
+/*//GMAIL
+$config['protocol'] = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.gmail.com';
+$config['smtp_user'] = 'user@gmail.com';
+$config['smtp_pass'] = '*******';
+$config['smtp_port'] = 465;
+*/
