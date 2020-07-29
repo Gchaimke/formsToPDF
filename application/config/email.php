0f7c40fc6ae3b935b1a9784cb6047c164e57ed5d
gchaim <gchaimke@gmail.com>
2020-07-29 10:20:55 +0300

    smtp settings

---
+++ application/config/email.php
@@ -0,0 +1,23 @@
+<?php
+$config['useragent'] = 'CodeIgniter';
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'mail.forms.garin.co.il';
+$config['smtp_user'] = 'forms@forms.garin.co.il';
+$config['smtp_port'] = 993;
+$config['smtp_timeout'] = 5;
+$config['wordwrap'] = TRUE;
+$config['wrapchars'] = 76;
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['validate'] = FALSE;
+$config['priority'] = 3;
+$config['crlf'] = "\r\n";
+$config['newline'] = "\r\n";
+$config['bcc_batch_mode'] = FALSE;
+$config['bcc_batch_size'] = 200;
+
+//GMAIL
+//$config['smtp_host']    = 'ssl://smtp.gmail.com';
+//$config['smtp_user'] = 'user@gmail.com';
+//$config['smtp_pass'] = '*******';
+//$config['smtp_port'] = 465;
