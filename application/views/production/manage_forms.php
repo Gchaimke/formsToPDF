290298d44d2381bef95d5f602929460aa9b857c1
gchaim <gchaimke@gmail.com>
2020-08-30 15:43:32 +0300

    csv to admin only

--- application/views/production/manage_forms.php
+++ application/views/production/manage_forms.php
@@ -12,90 +12,92 @@ if (isset($this->session->userdata['logged_in'])) {
 			</center>
 		</div>
 	</div>
-	<a id="show_csv"  href='#' class='btn btn-outline-info'><i class="fa fa-file-excel-o"></i></a>
-	<div id="csv_month" style="display:none;">
-	<?php for($i = 1;$i<13;$i++){
-		echo "<a target='blank' href='/production/export_to/$i' class='btn btn-outline-info'>$i</a><br/>";
-	}
-	?>
-	</div>
-	<div class="container rtl col-md-10">
-		<?php
-		if (isset($message_display)) {
-			echo "<div class='alert alert-success' role='alert'>";
-			echo $message_display . '</div>';
+	<?php if ($user_role == "Admin") { ?>
+		<a id="show_csv" href='#' class='btn btn-outline-info'><i class="fa fa-file-excel-o"></i></a>
+		<div id="csv_month" style="display:none;">
+		<?php for ($i = 1; $i < 13; $i++) {
+			echo "<a target='blank' href='/production/export_to/$i' class='btn btn-outline-info'>$i</a><br/>";
 		}
+	} //end if
 		?>
-		<nav aria-label="Checklist navigation">
-			<?php if (isset($links)) {
-				echo $links;
-			} ?>
-		</nav>
-		<form id="form" class='ltr'>
-			<div class="input-group mb-3">
-				<input id='inputSearch' type="text" class="form-control" placeholder="מספר תקלה,מספר לקוח,שם לקוח,יוצר" aria-label="Search in forms" aria-describedby="basic-addon2" autofocus>
-				<div class="input-group-append">
-					<button class="btn btn-outline-primary" type="button" onclick="formSearch()">חפש</button>
+		</div>
+		<div class="container rtl col-md-10">
+			<?php
+			if (isset($message_display)) {
+				echo "<div class='alert alert-success' role='alert'>";
+				echo $message_display . '</div>';
+			}
+			?>
+			<nav aria-label="Checklist navigation">
+				<?php if (isset($links)) {
+					echo $links;
+				} ?>
+			</nav>
+			<form id="form" class='ltr'>
+				<div class="input-group mb-3">
+					<input id='inputSearch' type="text" class="form-control" placeholder="מספר תקלה,מספר לקוח,שם לקוח,יוצר" aria-label="Search in forms" aria-describedby="basic-addon2" autofocus>
+					<div class="input-group-append">
+						<button class="btn btn-outline-primary" type="button" onclick="formSearch()">חפש</button>
+					</div>
 				</div>
-			</div>
-			<div id='searchResult' class='rtl text-center'></div>
-		</form>
-		<?php if (isset($results)) { ?>
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">תאריך</th>
-						<th scope="col">יוצר</th>
-						<th scope="col" class="mobile-hide">מספר לקוח</th>
-						<th scope="col" class="mobile-hide">שם הלקוח</th>
-						<th scope="col" class="mobile-hide">מיקום</th>
-						<th scope="col" class="mobile-hide">סוג תקלה</th>
-						<th scope="col" class="mobile-hide">חברה נותנת שירות</th>
-						<th scope="col">ערוך </th>
-						<?php if ($user_role == "Admin") {
-							echo '<th scope="col">מחק</th>';
-						}
-						?>
-					</tr>
-				</thead>
-				<tbody>
-
-					<?php foreach ($results as $data) {
-						if ($user_role != 'Admin' && $data->creator_id != $user_id)
-							continue;
-					?>
-						<tr id='<?php echo $data->id ?>'>
-							<td class="align-middle">
-								<?php
-								echo date("d-m-Y", strtotime($data->date));
-								if ($data->attachments != '') {
-									echo '<i class="mr-1 fa fa-paperclip" aria-hidden="true"></i> ';
-								}?>
-							</td>
-							<?php foreach ($users as $user) {
-								if ($user['id'] == $data->creator_id) {
-									echo '<td class="align-middle">' . $user['view_name'] . '</td>';
-								}
-							} ?>
-							<td class="mobile-hide align-middle"><?php echo $data->client_num ?></td>
-							<td class="mobile-hide align-middle"><?php echo $data->client_name ?></td>
-							<td class="mobile-hide align-middle"><?php echo $data->place ?></td>
-							<td class="mobile-hide align-middle"><?php echo $data->issue_kind ?></td>
-							<td class="mobile-hide align-middle"><?php echo $data->company ?></td>
-							<td><a href='/production/view_form/<?php echo $data->id ?>' class='btn btn-outline-info'><i class="fa fa-edit"></i></a></td>
+				<div id='searchResult' class='rtl text-center'></div>
+			</form>
+			<?php if (isset($results)) { ?>
+				<table class="table">
+					<thead class="thead-dark">
+						<tr>
+							<th scope="col">תאריך</th>
+							<th scope="col">יוצר</th>
+							<th scope="col" class="mobile-hide">מספר לקוח</th>
+							<th scope="col" class="mobile-hide">שם הלקוח</th>
+							<th scope="col" class="mobile-hide">מיקום</th>
+							<th scope="col" class="mobile-hide">סוג תקלה</th>
+							<th scope="col" class="mobile-hide">חברה נותנת שירות</th>
+							<th scope="col">ערוך </th>
 							<?php if ($user_role == "Admin") {
-								echo "<td><button id='" . $data->id . "' class='btn btn-outline-danger' onclick='deleteForm(this.id)'><i class='fa fa-trash'></i></button></td>";
+								echo '<th scope="col">מחק</th>';
 							}
 							?>
-
 						</tr>
-					<?php } ?>
-				</tbody>
-			</table>
-		<?php } else { ?>
-			<div>אין עדיין דוחות</div>
-		<?php } ?>
-	</div>
+					</thead>
+					<tbody>
+
+						<?php foreach ($results as $data) {
+							if ($user_role != 'Admin' && $data->creator_id != $user_id)
+								continue;
+						?>
+							<tr id='<?php echo $data->id ?>'>
+								<td class="align-middle">
+									<?php
+									echo date("d-m-Y", strtotime($data->date));
+									if ($data->attachments != '') {
+										echo '<i class="mr-1 fa fa-paperclip" aria-hidden="true"></i> ';
+									} ?>
+								</td>
+								<?php foreach ($users as $user) {
+									if ($user['id'] == $data->creator_id) {
+										echo '<td class="align-middle">' . $user['view_name'] . '</td>';
+									}
+								} ?>
+								<td class="mobile-hide align-middle"><?php echo $data->client_num ?></td>
+								<td class="mobile-hide align-middle"><?php echo $data->client_name ?></td>
+								<td class="mobile-hide align-middle"><?php echo $data->place ?></td>
+								<td class="mobile-hide align-middle"><?php echo $data->issue_kind ?></td>
+								<td class="mobile-hide align-middle"><?php echo $data->company ?></td>
+								<td><a href='/production/view_form/<?php echo $data->id ?>' class='btn btn-outline-info'><i class="fa fa-edit"></i></a></td>
+								<?php if ($user_role == "Admin") {
+									echo "<td><button id='" . $data->id . "' class='btn btn-outline-danger' onclick='deleteForm(this.id)'><i class='fa fa-trash'></i></button></td>";
+								}
+								?>
+
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			<?php } else { ?>
+				<div>אין עדיין דוחות</div>
+			<?php } ?>
+		</div>
 </main>
 <script>
 	function deleteForm(id) {
@@ -108,7 +110,7 @@ if (isset($this->session->userdata['logged_in'])) {
 			});
 		}
 	}
-	$('#show_csv').click(function(){
+	$('#show_csv').click(function() {
 		$('#csv_month').toggle();
 	});
 
