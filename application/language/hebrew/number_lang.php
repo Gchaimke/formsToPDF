bce3350215e3b1b68891ca7c11da3da98b9bded6
chaim <gchaimke@gmail.com>
2021-02-06 21:46:07 +0200

    system hebrew lang fix

--- application/language/hebrew/number_lang.php
+++ application/language/hebrew/number_lang.php
@@ -37,8 +37,8 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['terabyte_abbr'] = 'TB';
-$lang['gigabyte_abbr'] = 'GB';
-$lang['megabyte_abbr'] = 'MB';
-$lang['kilobyte_abbr'] = 'KB';
-$lang['bytes'] = 'Bytes';
+$lang['terabyte_abbr'] = 'טב';
+$lang['gigabyte_abbr'] = 'גב';
+$lang['megabyte_abbr'] = 'מב';
+$lang['kilobyte_abbr'] = 'קב';
+$lang['bytes'] = 'בייט';
--- application/language/hebrew/upload_lang.php
+++ application/language/hebrew/upload_lang.php
@@ -37,19 +37,19 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['upload_userfile_not_set'] = 'Unable to find a post variable called userfile.';
-$lang['upload_file_exceeds_limit'] = 'The uploaded file exceeds the maximum allowed size in your PHP configuration file.';
-$lang['upload_file_exceeds_form_limit'] = 'The uploaded file exceeds the maximum size allowed by the submission form.';
-$lang['upload_file_partial'] = 'The file was only partially uploaded.';
-$lang['upload_no_temp_directory'] = 'The temporary folder is missing.';
-$lang['upload_unable_to_write_file'] = 'The file could not be written to disk.';
-$lang['upload_stopped_by_extension'] = 'The file upload was stopped by extension.';
-$lang['upload_no_file_selected'] = 'You did not select a file to upload.';
-$lang['upload_invalid_filetype'] = 'The filetype you are attempting to upload is not allowed.';
-$lang['upload_invalid_filesize'] = 'The file you are attempting to upload is larger than the permitted size.';
-$lang['upload_invalid_dimensions'] = 'The image you are attempting to upload doesn\'t fit into the allowed dimensions.';
-$lang['upload_destination_error'] = 'A problem was encountered while attempting to move the uploaded file to the final destination.';
+$lang['upload_userfile_not_set'] = 'לא ניתן למצוא משתנה פוסט שנקרא userfile.';
+$lang['upload_file_exceeds_limit'] = 'הקובץ שהועלה חורג מהגודל המרבי המותר בקובץ התצורה של PHP.';
+$lang['upload_file_exceeds_form_limit'] = 'הקובץ שהועלה חורג מהגודל המרבי המותר בטופס ההגשה.';
+$lang['upload_file_partial'] = 'הקובץ הועלה באופן חלקי בלבד.';
+$lang['upload_no_temp_directory'] = 'התיקיה הזמנית חסרה.';
+$lang['upload_unable_to_write_file'] = 'לא ניתן היה לכתוב את הקובץ לדיסק.';
+$lang['upload_stopped_by_extension'] = 'העלאת הקובץ הופסקה על ידי סיומת.';
+$lang['upload_no_file_selected'] = 'לא בחרת קובץ להעלאה.';
+$lang['upload_invalid_filetype'] = 'סוג הקובץ שאתה מנסה להעלות אינו מורשה.';
+$lang['upload_invalid_filesize'] = 'הקובץ שאתה מנסה להעלות גדול מהגודל המותר.';
+$lang['upload_invalid_dimensions'] = 'התמונה שאתה מנסה להעלות לא מתאימה למידות המותרות.';
+$lang['upload_destination_error'] = 'אירעה בעיה בעת ניסיון להעביר את הקובץ שהועלה ליעד הסופי.';
 $lang['upload_no_filepath'] = 'The upload path does not appear to be valid.';
-$lang['upload_no_file_types'] = 'You have not specified any allowed file types.';
-$lang['upload_bad_filename'] = 'The file name you submitted already exists on the server.';
-$lang['upload_not_writable'] = 'The upload destination folder does not appear to be writable.';
+$lang['upload_no_file_types'] = 'לא ציינת סוגי קבצים מורשים.';
+$lang['upload_bad_filename'] = 'שם הקובץ שהגשת כבר קיים בשרת.';
+$lang['upload_not_writable'] = 'נראה שתיקיית היעד של העלאה אינה ניתנת לכתב.';
